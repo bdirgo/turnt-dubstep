5df94e8aaa5d33c192c535566b67cc1bbc39e481
Benjamin Dirgo <bdirgo@gmail.com>
2014-05-28 23:29:31 +0000

    added a default rails website

---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '8bcc0d1c4696af3f2229cc6d7d52ac4d2ee694299a20f201c9000444c0268345cdb7e37d26ffecfff9887c773edc224556800015bc35a1f6e159ca64403b80ab'
